f905e1694b1b3f3e4a6e98235066bb7bd8b6d458
Boris Sterligov <bsterligov@gmail.com>
2024-12-21 12:45:05 +0100

    day3 part2

---
+++ day-3/script_p2.py
@@ -0,0 +1,33 @@
+data = open("input.txt", "r")
+data  = data.read()
+dont = "don't()"
+do = 'do()'
+
+def check_start(element):
+    if element[0] == "(":
+          return True  
+    return False
+
+def execute(data):
+    mul  = data.split("mul")
+    sum = 0
+    for item in mul:
+        index = item.index(')') if ')' in item else -1
+        if index != -1:
+            sp = (item[1:index]).split(",")
+            if (len(sp) == 2 and sp[0].isdigit() and sp[1].isdigit()):
+                sum += int(sp[0]) * int(sp[1])
+    return sum
+
+sum = 0
+left = 0
+right = data.index(dont) if dont in data else len(data)
+
+while right < len(data) and len(data) != 0:
+    sum += execute(data[left:right])
+    
+    data =  data[right+len(dont):len(data)]
+    left = data.index(do) if do in data else len(data)
+    right = data.index(dont) if dont in data else len(data)
+
+print(sum)
