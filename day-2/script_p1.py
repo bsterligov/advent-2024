bacc020421ee021def1b97eac4344a3461a17b4c
Boris Sterligov <bsterligov@gmail.com>
2024-12-21 12:05:22 +0100

    day2 part1

---
+++ day-2/script_p1.py
@@ -0,0 +1,21 @@
+data = open("input.txt", "r")
+
+safe = 0;
+data  = data.read().split("\n")
+for line in data:
+    line = list(filter(None, line.split(" ")))
+    if (len(line) == 0):
+        continue
+    is_safe = True
+    p = (int(line[0]) - int(line[1]) > 0)
+    for i in range(len(line)-1):
+        i1 = int(line[i])
+        i2 = int(line[i+1])
+        diff = i1 - i2 > 0
+        if (p != diff) or (i1 == i2) or (abs(i1 - i2) > 3):
+            is_safe = False
+            break
+    if (is_safe):
+        safe += 1
+
+print(safe)
