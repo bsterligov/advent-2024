14aba68f5fe333ac3813af60999ff2a0065d9850
Boris Sterligov <bsterligov@gmail.com>
2024-12-22 10:10:22 +0100

    day7 part1

---
+++ day-7/script_p1.py
@@ -0,0 +1,29 @@
+data = open("input.txt", "r")
+
+
+def equals(sum, numbers):
+    def backtrack(index, current_value):
+        if index == len(numbers):
+            return current_value == sum
+
+        if backtrack(index + 1, current_value + numbers[index]):
+            return True
+
+        if backtrack(index + 1, current_value * numbers[index]):
+            return True
+
+        return False
+
+    return backtrack(1, numbers[0])
+
+
+result = 0
+for line in data:
+    eval, numbers = line.strip().split(": ")
+
+    eval = int(eval)
+    numbers = list(map(int, numbers.split()))
+
+    result += eval if equals(eval, numbers) else 0
+
+print(result)
